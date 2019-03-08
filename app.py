6f939e9644cdade317336732cf5eb84d16f0cb77
Yosry Mohammad <yosrym93@gmail.com>
2019-03-09 01:54:23 +0200

    changed documentation page title and default view

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_restplus import Api
 
 app = Flask(__name__)
-api = Api(app, doc='/api/doc')
+api = Api(app, doc='/api/doc', title='Kwikker API')
 create_model = api.model
 app.config['RESTPLUS_MASK_SWAGGER'] = False
-
+app.config['SWAGGER_UI_DOC_EXPANSION'] = 'list'
 
 
