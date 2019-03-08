45216c5d72aacec2e522052f6192e9b6740d0c37
unknown <iamadeadfish@hotmail.com>
2019-03-09 01:51:38 +0200

    added Version parameter

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restplus import Api
 
 app = Flask(__name__)
-api = Api(app, doc='/api/doc')
+api = Api(app, doc='/api/doc',version='1.0')
 app.config['RESTPLUS_MASK_SWAGGER'] = False
 
 
