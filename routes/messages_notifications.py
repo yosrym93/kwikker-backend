08c05b6a127e2bc951401a858201e697805dd3ad
unknown <iamadeadfish@hotmail.com>
2019-03-09 18:57:16 +0200

    added parameter for [notificaiton,conversation]&& fixing coding style

--- routes/messages_notifications.py
+++ routes/messages_notifications.py
@@ -11,25 +11,25 @@ class DirectMessages(Resource):
     @messages_api.response(code=200, description='Messages Returned Successfully.', model=[DirectMessage.api_model])
     @messages_api.response(code=401, description='Unauthorized[User does not have enough privilege].')
     @messages_api.response(code=404, description='User does not exist.')
-    @messages_api.param(name='Username', type="String", description='Username for Messages to display', required=True)
-    @messages_api.param(name='Last_Message_Retrieved. ', type="String", description='Id of the last message retrieved '
-                                                                                   ',generally 20 messages are sent if'
-                                                                                   ' null ,but can retrieve more by '
-                                                                                   'sending id of the last message.'
-                                                                                   '', nullable=True)
+    @messages_api.param(name='username', type="str", description='Username for Messages to display', required=True)
+    @messages_api.param(name='last_retrieved_message_id. ', type="str", description='Nullable.Normally the request '
+                                                                                    'returns the first 20 messages when'
+                                                                                    ' null.To retrieve more send the id'
+                                                                                    ' of the last message retrieved.',
+                        nullable=True)
     def get(self):
         ''' Retrieves a list of Direct Messages '''
         pass
 
-    @messages_api.response(code=201, description='Message Created Successfully.', model=[DirectMessage.api_model])
+    @messages_api.response(code=201, description='Message Created Successfully.')
     @messages_api.response(code=401, description='Unauthorized[User does not have enough privilege].')
     @messages_api.response(code=404, description='User does not exist.')
     @messages_api.expect(create_model('Message', model={
-        'Text': fields.String(description='The content of the message.'),
-        'Username': fields.String(description='Username ,message was send to.')
+        'text': fields.String(description='The content of the message.'),
+        'username': fields.String(description='Username ,message was send to.')
     }))
     def post(self):
-        ''' Creates a New Direct Message. '''
+        """ Creates a New Direct Message. """
         pass
 
 
@@ -37,8 +37,14 @@ class DirectMessages(Resource):
 class Conversations(Resource):
     @messages_api.response(code=200, description='Conversations Returned Successfully.', model=[Conversation.api_model])
     @messages_api.response(code=401, description='Unauthorized[User does not have enough privilege].')
+    @messages_api.param(name='last_retrieved_conversations_id. ', type="str", description='Nullable.Normally the '
+                                                                                          'request returns the first 20'
+                                                                                          ' conversations when null.To '
+                                                                                          'retrieve more send the id of'
+                                                                                          ' the last conversations '
+                                                                                          'retrieved.', nullable=True)
     def get(self):
-        ''' Retrieves a list of Conversations. '''
+        """ Retrieves a list of Conversations. """
         pass
 
 
@@ -47,6 +53,14 @@ class Notifications(Resource):
     @notifications_api.response(code=200, description='Notifications Returned Successfully.', model=[Notification.
                                 api_model])
     @notifications_api.response(code=401, description='Unauthorized[User does not have enough privilege].')
+    @notifications_api.param(name='last_notifications_retrieved_id. ', type="str", description='Nullable.Normally the '
+                                                                                               'request returns the '
+                                                                                               'first 20 notifications '
+                                                                                               'when null.To retrieve'
+                                                                                               ' more send the id of '
+                                                                                               'the last notifications'
+                                                                                               ' retrieved.',
+                             nullable=True)
     def get(self):
-        ''' Retrieves a list of Notifications. '''
+        """ Retrieves a list of Notifications. """
         pass
