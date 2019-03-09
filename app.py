f7a7516f8c651c6e1c6d200eeab3d2d769e9c5ad
unknown <iamadeadfish@hotmail.com>
2019-03-09 02:42:09 +0200

    added version parameter

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask_restplus import Api
 
 app = Flask(__name__)
-api = Api(app, doc='/api/doc',version='1.0')
+api = Api(app, doc='/api/doc', title='Kwikker API',version='1.0')
+create_model = api.model
 app.config['RESTPLUS_MASK_SWAGGER'] = False
-
+app.config['SWAGGER_UI_DOC_EXPANSION'] = 'list'
 
 
