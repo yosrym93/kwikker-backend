a67bf7c286c3d75afa62c99da9d9b2fad20e118c
unknown <iamadeadfish@hotmail.com>
2019-03-17 15:16:27 +0200

    added some function documentation which works with sphinx software

--- notifications/actions.py
+++ notifications/actions.py
@@ -7,13 +7,56 @@ import datetime
 
 
 def get_notifications(username):
+    """
+        this function get list of notifications for a give username
+
+        **PARAMETER**:
+
+            * *involved_username*: user who is reposonsible for the notification
+            * *type_notification*: type of the notification [FOLLOW-REKWEEK-LIKE]
+            * *kweek_id*: the id of the kweek involved
+
+        **RETURNS**:
+
+            - list of notifications for the users in json format
+
+
+
+        """
     return query_factory.get_notifications(username)
 
 
 def create_notifications(involved_username, type_notification, kweek_id):
+    """
+     this function create a notification in the database.
+
+     **PARAMETER**:
+
+         * *involved_username*: user who is reposonsible for the notification
+         * *type_notification*: type of the notification [FOLLOW-REKWEEK-LIKE]
+         * *kweek_id*: the id of the kweek involved
+
+     **RETURNS**:
+
+         - doesn't return anything
+
+
+     """
     return query_factory.create_notifications(involved_username, type_notification, kweek_id, datetime.datetime.now())
 
 
 # function for testing
 def get_list_size_notification():
+    """
+        this function count all notifications in database
+
+        **PARAMETER**:
+
+            - no parameter
+
+        **RETURNS**:
+            - number of notifications in the database
+
+
+        """
     return query_factory.get_list_size_notification()
