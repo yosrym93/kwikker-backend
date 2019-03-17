619cc570e960e581db7bf1b84afccb2a312866be
khaled galal <khaloudfire104@gamil.com>
2019-03-17 17:10:14 +0200

    User_Profile actions and queries completed working on documentation and testing

--- users_profiles/query_factory.py
+++ users_profiles/query_factory.py
@@ -1,5 +1,4 @@
 import database_manager
-
 db_manager = database_manager.db_manager
 
 """
@@ -14,3 +13,93 @@ db_manager = database_manager.db_manager
     The return of a SELECT query is a list of dictionaries, where each row is represented by a dictionary.
     The keys of the dictionary are the database column names.
 """
+
+
+def get_user_profile(username):
+    query: str = """
+                     select * from profile where username=%s 
+
+                 """
+    data = (username,)
+    response = db_manager.execute_query(query, data)[0]
+    return response
+
+
+def update_user_profile(username, bio, screen_name):
+        if bio == '' or bio is None:
+            query: str = """
+                                update profile set screen_name = %s where username = %s
+                         """
+            data = (screen_name, username)
+            response = db_manager.execute_query_no_return(query, data)
+            return response
+
+        elif screen_name == '':
+            query: str = """
+                                update profile set bio = %s  where username = %s
+                         """
+            data = (bio, username)
+            response = db_manager.execute_query_no_return(query, data)
+            return response
+
+        else:
+            query: str = """
+                                update profile set bio = %s , screen_name = %s where username = %s
+                         """
+            data = (bio, screen_name, username)
+            response = db_manager.execute_query_no_return(query, data)
+            return response
+
+
+def update_user_profile_picture(username, filename):
+    query: str = """
+                        update profile set profile_image_url = %s where username = %s
+                 """
+    data = (filename, username)
+    response = db_manager.execute_query_no_return(query, data)
+    return response
+
+
+def update_user_banner_picture(username, filename):
+    query: str = """
+                      update profile set profile_banner_url = %s where username = %s
+                 """
+    data = (filename, username)
+    response = db_manager.execute_query_no_return(query, data)
+    return response
+
+
+def get_user_followers(username):
+    query: str = """
+                         select count(*) from follow where follower_username = %s
+                 """
+    data = (username,)
+    response = db_manager.execute_query(query, data)[0]
+    return response
+
+
+def get_user_following(username):
+    query: str = """
+                         select count(*) from follow where followed_username = %s
+                 """
+    data = (username,)
+    response = db_manager.execute_query(query, data)[0]
+    return response
+
+
+def get_number_of_kweeks(username):
+    query: str = """
+                         select count(*) from kweek where username = %s
+                 """
+    data = (username,)
+    response = db_manager.execute_query(query, data)[0]
+    return response
+
+
+def get_number_of_likes(username):
+    query: str = """
+                        select count(*) from favorite where username = %s
+                 """
+    data = (username,)
+    response = db_manager.execute_query(query, data)[0]
+    return response
--- users_profiles/routes.py
+++ users_profiles/routes.py
@@ -1,7 +1,9 @@
-from flask_restplus import Resource, fields
+from flask_restplus import Resource, fields , abort
+from flask import request, send_from_directory
 from models import User, UserProfile
 from app import create_model
 import api_namespaces
+from .import actions
 
 user_api = api_namespaces.user_api
 interactions_api = api_namespaces.interactions_api
@@ -29,7 +31,12 @@ class ProfileBanner(Resource):
     @user_api.response(code=404, description='Delete failed.')
     def delete(self,):
         """ Delete a profile banner (restores the default one). """
-        pass
+        authorized_username = 'khaled'  # waiting for function
+        response = actions.delete_banner_picture(authorized_username)
+        if response == - 1:
+            return '', 404
+        url = 'http://127.0.0.1:5000/user/upload/banner/banner.png'
+        return url, 200
 
     @user_api.response(code=200, description='Profile banner updated.')
     @user_api.response(code=404, description='Update failed.')
@@ -38,7 +45,14 @@ class ProfileBanner(Resource):
     @user_api.param(name='image_file', description='The new profile banner.', required=True, type='file')
     def put(self):
         """ Update a profile banner given the new banner image. """
-        pass
+        file = request.files['file']
+        authorized_username = 'khaled'  # waiting for function
+        response = actions.update_profile_banner(file, authorized_username)
+        if response == - 1:
+            return '', 404
+        url = 'http://127.0.0.1:5000/user/upload/banner/'
+        url = url + response
+        return url, 200
 
 
 @user_api.route('/profile_picture')
@@ -48,16 +62,41 @@ class ProfilePicture(Resource):
     @user_api.response(code=401, description='Unauthorized access.')
     def delete(self,):
         """ Delete a profile picture (restores the default one). """
-        pass
+        authorized_username = 'khaled'  # waiting for function
+        response = actions.delete_profile_picture(authorized_username)
+        if response == - 1:
+            return '', 404
+        url = 'http://127.0.0.1:5000/user/upload/picture/profile.jpg'
+        return url, 200
 
     @user_api.response(code=200, description='Profile picture updated.')
     @user_api.response(code=404, description='Update failed.')
     @user_api.response(code=400, description='Parameters type does not match.')
     @user_api.response(code=401, description='Unauthorized access.')
     @user_api.param(name='image_file', description='The new profile picture.', required=True, type='file')
-    def put(self):
+    def post(self):
         """ Update a profile picture given the new picture. """
-        pass
+        # for file in request.files.getlist("file"):
+        file = request.files['file']
+        authorized_username = 'khaled'  # waiting for function
+        response = actions.update_profile_picture(file, authorized_username)
+        if response == - 1:
+            return '', 404
+        url = 'http://127.0.0.1:5000/user/upload/picture/'
+        url = url + response
+        return url, 200
+
+
+@user_api.route('/upload/picture/<filename>')
+class PhotoUploadP(Resource):
+    def get(self, filename):
+        return send_from_directory('users_profiles\images\profile', filename)
+
+
+@user_api.route('/upload/banner/<filename>')
+class PhotoUploadB (Resource):
+    def get(self, filename):
+        return send_from_directory('users_profiles\images\ banner', filename)
 
 
 @user_api.route('/profile')
@@ -71,16 +110,31 @@ class UserProfile(Resource):
     }))
     def patch(self):
         """ Update the biography or screen name in user profile."""
-        pass
+        authorized_username = 'khaled'  # waiting for function
+        data = request.get_json()
+        bio = data.get('bio')
+        screen_name = data.get('screen_name')
+        response = actions.update_user_profile(authorized_username, bio, screen_name)
+        if response == - 1:
+            abort(404, message='update failed.')
+        if response == 0:
+            return '', 400
+        return '', 200
 
     @user_api.response(code=200, description='User profile returned successfully.', model=UserProfile.api_model)
     @user_api.response(code=404, description='User does not exist.')
     @user_api.response(code=401, description='Unauthorized access.')
     @user_api.response(code=400, description='Parameters type does not match.')
+    @user_api.marshal_with(UserProfile.api_model, as_list=True)
     @user_api.param(name='username', type='str', required=True, description='The username.' )
     def get(self):
         """ Retrieve the profile of a specific user. """
-        pass
+        username = request.args.get('username')  #  user to be sent from front
+        authorized_username = 'khaled'  # waiting for function
+        response = actions.get_user_profile(authorized_username, username)
+        if response == - 1:
+            return '', 404
+        return response, 200
 
 
 @user_api.route('/email')
