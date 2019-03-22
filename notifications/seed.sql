ba216d91f10d51de664827c7372a8c320a9d37e0
Yosry Mohammad <yosrym93@gmail.com>
2019-03-22 23:27:45 +0200

    Updated notifications testing seed

--- notifications/seed.sql
+++ notifications/seed.sql
@@ -7,18 +7,18 @@ INSERT INTO USER_CREDENTIALS
 
 
 INSERT INTO public.profile(
-	username, screen_name, profile_image_url, profile_banner_url, bio, created_at)
-	VALUES ('ahly','ahlyscreenname','ahly.profile.com','ahly.banner.com','ahly bio','1998-3-3 00:00:00'),
-		   ('zamalek','zamalekscreenname','zamalek.profile.com','zamalek.banner.com','zamalek bio','2001-5-5 5:7:9'),
-		   ('arsenal','arsenalscreenname','arsenal.profile.com','arsenal.banner.com','arsenal bio','1901-5-5 5:5:5'),
-	       ('kitchen','kitchenscreenname','kitchen.profile.com','kitchen.banner.com','kitchen bio','2015-1-1 4:3:6');
+	username, screen_name, profile_image_url, profile_banner_url, bio, created_at, birth_date)
+	VALUES ('ahly','ahlyscreenname','ahly.profile.com','ahly.banner.com','ahly bio','1998-3-3 00:00:00', '2000-01-01'),
+		   ('zamalek','zamalekscreenname','zamalek.profile.com','zamalek.banner.com','zamalek bio','2001-5-5 5:7:9', '2000-01-01'),
+		   ('arsenal','arsenalscreenname','arsenal.profile.com','arsenal.banner.com','arsenal bio','1901-5-5 5:5:5', '2000-01-01'),
+	       ('kitchen','kitchenscreenname','kitchen.profile.com','kitchen.banner.com','kitchen bio','2015-1-1 4:3:6', '2000-01-01');
 
 
 INSERT INTO public.kweek(
 	created_at, text, media_url, username, reply_to)
-	VALUES ('2010-1-1', 'kweek of 2010','www.media_url.2010','ahly',3),
-		   ('2014-4-4', 'kweek of 2014','www.media_url.2014','zamalek',2),
-		   ('2018-7-5', 'kweek of 2014','www.media_url.2018','kitchen',1);
+	VALUES ('2010-1-1', 'kweek of 2010','www.media_url.2010','ahly',null),
+		   ('2014-4-4', 'kweek of 2014','www.media_url.2014','zamalek',null),
+		   ('2018-7-5', 'kweek of 2014','www.media_url.2018','kitchen',null);
 
 
 INSERT INTO public.notification(
