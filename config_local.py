6eb71c5259ea10a885cba8f22c3873bb7fe28c02
Yosry Mohammad <33295400+yosrym93@users.noreply.github.com>
2019-04-13 14:47:57 +0200

    Update config_local.py

--- config_local.py
+++ config_local.py
@@ -1,6 +1,3 @@
-# Add your local configurations here, this file will not be tracked by git
-# SERVER_PATH is used for generating image urls
-
 # SERVER_PATH = 'http://6d5bcddc.ngrok.io/'
 DATABASE_USERNAME = 'postgres'
 DATABASE_PASSWORD = '1'
