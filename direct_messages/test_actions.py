50be1541403180893288dcfd47a60524b94f4321
Omar <IamAdeadfish@hotmail.com>
2019-04-13 17:24:45 +0200

    added unit testing of direct messages

--- direct_messages/test_actions.py
+++ direct_messages/test_actions.py
@@ -17,7 +17,7 @@ def get_id(from_username, to_username):
                     WHERE FROM_USERNAME = %s
                     AND   TO_USERNAME = %s
                     
-                    ORDER BY CREATED_AT DES
+                    ORDER BY CREATED_AT DESC
                     LIMIT 1
             """
     data = (from_username, to_username, to_username, from_username)
@@ -26,25 +26,7 @@ def get_id(from_username, to_username):
 
 
 def count_message(from_username, to_username):
-    query = """SELECT ID, FROM_USERNAME, TO_USERNAME, CREATED_AT, TEXT, MEDIA_URL
-                    FROM MESSAGE
-                    WHERE FROM_USERNAME = %s
-                    AND   TO_USERNAME = %s
-
-                    UNION
-
-                    SELECT ID, FROM_USERNAME, TO_USERNAME, CREATED_AT, TEXT, MEDIA_URL
-                    FROM MESSAGE
-                    WHERE FROM_USERNAME = %s
-                    AND   TO_USERNAME = %s
-
-                    ORDER BY CREATED_AT DES
-            """
-    data = (from_username, to_username, to_username, from_username)
-    array = db_manager.execute_query(query, data)
-    length = len(array)
-    return length
-
+    return len(actions.get_messages(from_username,to_username))
 
 @pytest.mark.parametrize("test_username, expected_output, last_conversations_retrieved_id",
                          [
@@ -79,17 +61,17 @@ def test_get_conversations_empty():
     assert list_conversations == []
 
 
-@pytest.mark.parametrize("test_username, expected_output, last_message_retrieved_id",
+@pytest.mark.parametrize("from_username, to_username, expected_output, last_message_retrieved_id",
                          [
-                             ('ahly', 2, None),
-                             ('ahly', 1, get_id('ahly', 'zamalek')),
+                             ('ahly', 'zamalek', 2, None),
+                             ('ahly', 'zamalek', 1, get_id('ahly', 'zamalek')),
                          ])
-def test_get_messages(test_username, expected_output, last_message_retrieved_id):
+def test_get_messages(from_username, to_username, expected_output, last_message_retrieved_id):
     """
         this function tests if list of messages is returned properly by checking size of the list
     """
-    list_conversations = actions.get_conversations(test_username, last_message_retrieved_id)
-    count = len(list_conversations)
+    list_messages = actions.get_messages(from_username, to_username, last_message_retrieved_id)
+    count = len(list_messages)
     assert count == expected_output
 
 
@@ -99,7 +81,7 @@ def test_get_messages_value_error():
     """
     exception_caught = False
     try:
-        actions.get_messages('ahly', 'invalid_id')
+        actions.get_messages('ahly', 'zamalek', 'invalid_id')
     except ValueError:
         exception_caught = True
     assert exception_caught is True
@@ -109,7 +91,7 @@ def test_get_messages_empty():
     """
         this function tests exception when messages return empty list
     """
-    list_messages = actions.get_messages('no_message', None)
+    list_messages = actions.get_messages('no_message', 'zamalek', None)
     assert list_messages == []
 
 
@@ -117,9 +99,9 @@ def test_create_message():
     """
         this function tests if list of messages is created properly by checking size of the list
     """
-    old_size = count_message()
+    old_size = count_message('ahly','zamalek')
     actions.create_message('ahly', 'zamalek','test message',None)
-    new_size = count_message()
+    new_size = count_message('ahly','zamalek')
     assert new_size == (old_size + 1)
 
 
@@ -138,7 +120,7 @@ def test_create_message_to_user_error():
         this function tests exception error of notified_user
     """
     try:
-        actions.create_message('zamlek', 'qq', 'test message',None)
+        actions.create_message('zamalek', 'qq', 'test message',None)
     except Exception as E:
         assert str(E) == 'Username who want to receive this message does not exist.'
 
@@ -156,9 +138,27 @@ def test_get_conversationers(test_username, expected_output, last_conversationer
     assert count == expected_output
 
 
+def test_get_conversationers_not_exist():
+    """
+        this function tests if list of conversations is returned properly by checking size of the list
+    """
+    try:
+        actions.get_recent_conversationers('ahly', 'qq')
+    except Exception as E:
+        assert str(E) == 'Username does not exist.'
+
+
 def test_get_conversationers_empty():
     """
         this function tests exception when conversations return empty list
     """
     list_conversations = actions.get_recent_conversationers('no_message', None)
+    assert list_conversations == []
+
+
+def test_get_conversationers_user():
+    """
+        this function tests exception when conversations return empty list
+    """
+    list_conversations = actions.get_recent_conversationers('ahly', 'no_message')
     assert list_conversations == []
