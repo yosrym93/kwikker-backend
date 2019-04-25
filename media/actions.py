efb91a22817ec43af9a4f966dd78185b32131be9
Omar <IamAdeadfish@hotmail.com>
2019-04-25 22:46:34 +0200

    added media modules [without testing]

--- media/actions.py
+++ media/actions.py
@@ -1,6 +1,95 @@
 from . import query_factory
+import os, fnmatch
+from app import app
+import datetime
+from flask import send_from_directory
 
-"""
-    All the functions containing the logic should reside here. 
-    The routes functions should contain no logic, they should only call the functions in this module.
-"""
+APP_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+Server_path = app.config['SERVER_PATH']
+
+
+def create_url( filename):
+    """
+                    The function return url of the image .
+                    *Parameters*:
+                        - *filename (str)*: The name of the profile image.
+                    *Returns*:
+                        - url of image .
+    """
+    url = Server_path + 'media/get/'+ filename
+    return url
+
+
+def allowed_file(filename):
+    """
+        The function checks if the uploaded file has allowed extension.
+
+        *Parameters*:
+            - *filename*: The name of the uploaded file .
+        *Returns*:
+            - *True or False*: in the allowed extension or not.
+    """
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def save_file(file):
+    """
+        This function saves a certain file in the directory.
+
+        *Parameters*:
+            - *file (file)*: The image which will be updated.
+        *Returns*:
+            - *filename*: the image name saved in the media directory  .
+    """
+    if file.filename == '':
+        return 'No selected file'
+    if file and allowed_file(file.filename):
+        images = os.path.join(APP_ROOT, 'images/')
+        if not os.path.isdir(images):
+            os.mkdir(images)
+        target = os.path.join(APP_ROOT, 'images/media/')
+        if not os.path.isdir(target):
+            os.mkdir(target)
+        filename, ext = os.path.splitext(file.filename)
+        filename = str(datetime.datetime.now()).replace(" ","").replace("-","").replace(":","").replace(".","")
+        filename_no_ext=filename
+        filename+=ext
+        destination = "/".join([target, filename])
+        file.save(destination)
+        return filename_no_ext
+    else:
+        return 'not allowed extensions'
+
+
+def get_extension_file(filename):
+    """
+        this function gets the file extension from media directory.
+
+        *Parameters*:
+            - *filename (file)*: The filename to be searched for.
+        *Returns*:
+            - *filename*: file name with extension.
+    """
+
+    search_path = os.path.join(APP_ROOT, 'images/media')
+    filename = filename+ ".*"
+    for root, dirs, files in os.walk(search_path):
+        for name in files:
+            if fnmatch.fnmatch(name, filename):
+                return name
+    raise Exception("file is not found")
+
+
+def send_file(filename):
+    """
+    this function sends the file from media directory .
+
+    *Parameters*:
+        - *filename (file)*: The filename to be sent.
+    *Returns*:
+        - *filename*: file name with extension.
+    """
+
+    os.chdir(os.path.dirname(APP_ROOT))
+    return send_from_directory('images/media', filename)
--- media/routes.py
+++ media/routes.py
@@ -1,7 +1,11 @@
-from flask_restplus import Resource, fields
+from flask_restplus import Resource, fields, abort
+from flask import request, send_from_directory
 from app import create_model
+from .import actions
 import api_namespaces
-
+from authentication_and_registration.actions import authorize
+import os
+APP_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 media_api = api_namespaces.media_api
 
 
@@ -14,8 +18,35 @@ class Media(Resource):
                                                   fields.String(description='The id of the uploaded image.')}))
     @media_api.response(code=404, description='Media not found. Uploading failed.')
     @media_api.response(code=401, description='Unauthorized access.')
-    def post(self):
-            """
-            Post a new media file .
-            """
-            pass
+    @media_api.response(code=400, description='Not allowed extensions.')
+    @media_api.param(name='file', description='image file.', required=True, type='file')
+    @media_api.doc(security='KwikkerKey')
+    @authorize
+    def post(self,authorized_username):
+        """
+        Post a new media file. and return id of the file without extension
+        """
+        if 'file' not in request.files:
+            return abort(404, message='No image file')
+        file = request.files['file']
+        response = actions.save_file(file)
+        if response == 'No selected file':
+            return abort(404, message=response)
+        if response == 'not allowed extensions':
+            return abort(400, message=response)
+        return {"filename":response}, 200
+
+
+
+@media_api.route('/get/<filename>')
+class get_Media (Resource):
+    @staticmethod
+    def get(filename):
+        """ this endpoint gets the image given the url """
+        try:
+            filename =actions.get_extension_file(filename)
+            os.chdir(os.path.dirname(APP_ROOT))
+            return send_from_directory('images/media', filename)
+        except Exception as E:
+            if str(E) == "file is not found":
+                abort(404, message='file is not found.')
