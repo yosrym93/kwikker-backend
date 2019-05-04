921b4af6fc8c690cb2cbd38739fc8629665371ed
Omar <IamAdeadfish@hotmail.com>
2019-05-05 00:52:41 +0200

    fixed seed to be compatible with is_seen col in db

--- direct_messages/seed.sql
+++ direct_messages/seed.sql
@@ -16,13 +16,13 @@ INSERT INTO public.profile(
 	       ('no_message','no_mess_screenname','no_mess.profile.com','no_mess.banner.com','no_mess bio','2015-1-1 4:3:6', '2000-01-01');
 
 INSERT INTO public.message(
-	    from_username, to_username, created_at, text, media_url)
-	    VALUES ( 'ahly', 'zamalek', '2015-1-1 4:3:6', 'msg from ahly to zamalek','ahly-zamalek.mediaurl' ),
-	           ( 'zamalek', 'ahly', '2014-1-1 4:3:6', 'msg from zamalek to ahly','zamalek-ahly.mediaurl' ),
-	           ( 'ahly', 'arsenal', '2011-1-1 4:3:6', 'msg from ahly to arsenal','ahly-zamalek.mediaurl' ),
-	           ( 'arsenal', 'ahly', '2012-1-1 4:3:6', 'msg from arsenal to ahly','ahly-zamalek.mediaurl' ),
-	           ( 'ahly', 'degla', '2018-1-1 4:3:6', 'msg from ahly to degla','ahly-zamalek.mediaurl' ),
-	           ( 'degla', 'ahly', '2016-1-1 4:3:6', 'msg from degla to ahly','ahly-zamalek.mediaurl' );
+	    from_username, to_username, created_at, text, media_url,is_seen)
+	    VALUES ( 'ahly', 'zamalek', '2015-1-1 4:3:6', 'msg from ahly to zamalek','ahly-zamalek.mediaurl', FALSE ),
+	           ( 'zamalek', 'ahly', '2014-1-1 4:3:6', 'msg from zamalek to ahly','zamalek-ahly.mediaurl', TRUE ),
+	           ( 'ahly', 'arsenal', '2011-1-1 4:3:6', 'msg from ahly to arsenal','ahly-zamalek.mediaurl', TRUE ),
+	           ( 'arsenal', 'ahly', '2012-1-1 4:3:6', 'msg from arsenal to ahly','ahly-zamalek.mediaurl', TRUE ),
+	           ( 'ahly', 'degla', '2018-1-1 4:3:6', 'msg from ahly to degla','ahly-zamalek.mediaurl', TRUE ),
+	           ( 'degla', 'ahly', '2016-1-1 4:3:6', 'msg from degla to ahly','ahly-zamalek.mediaurl', TRUE );
 
 
 INSERT INTO public.follow(
